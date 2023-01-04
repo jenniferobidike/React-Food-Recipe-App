31108bd982efc53cacd95c454faf8fdc9a9d5842
Jennifer Obidike <thejoysofexistene@gmail.com>
2023-01-04 10:24:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Recipe from './Recipe';
+import { v4 as uuidv4 } from "uuid";
+
+  
+const App = () => {
+  const APP_ID = "7b03d467";
+  const APP_KEY = "97861c537243009076d018b07a6a991d";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('salad');
+
+  useEffect(() => {
+    getRecipes();
+  }, [query])
+
+  const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    //console.log(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
+  return (
+    <div className="App">
+      <h1>Food Searching App</h1>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button">
+          Search Food
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+          key={uuidv4()}
+          label={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+          />
+  
+        ))}
+      </div>
+    </div>
+
+  );
+};
+
+
+
+ 
+export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,24 @@
+import React from "react";
+import style from './recipe.module.css';
+import { v4 as uuidv4 } from "uuid";
+  
+const Recipe = ({label,image,url,ingredients}) =>{
+    return(
+        <div key={uuidv4()} className={style.recipe}>
+            <h2>{label}</h2>
+            <ul>
+                {ingredients.map(ingredient=>(
+                    <li key={uuidv4()}>{ingredient.text}</li>
+                ))}
+            </ul>
+            
+     <button className={style.url}><a className={style.link} href={url} target="_blank" rel="noopener noreferrer">
+        Recipe Details
+      </a></button>
+            <img className={style.image} src={image} alt="label"/>
+  
+        </div>
+    );
+  
+}
+export default Recipe;
